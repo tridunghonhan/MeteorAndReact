b72b7c90f6753e2dd211caa29e92a1ac7f06809a
KennyHo <geotech@KennyMacMini.local>
2016-10-06 18:03:21 +0700

    start using bootstrap

--- images/client/components/image_detail.js
+++ images/client/components/image_detail.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
-const ImageDetail = () => {
+const ImageDetail = (props) => {
+    //props stands for properties 
     return (
-        <div> Image Detail Here </div>
+        <li className="media list-group-item">
+            <div className="media-left"> 
+                <img src={props.image.link} />
+            </div>
+            <div className="media-body">
+                <h4 className="media-heading">
+                    {props.image.title}
+                </h4>
+            </div> 
+        </li>
     );
 };
 export default ImageDetail;
--- images/client/components/image_list.js
+++ images/client/components/image_list.js
@@ -2,18 +2,18 @@ import React from 'react';
 import ImageDetail from './image_detail';
 
 const IMAGES = [
-    {title: 'pen', link: 'http://dummyimage.com/600x400'},
+    {title: 'Pen', link: 'http://dummyimage.com/600x400'},
     {title: 'Pine Tree', link: 'http://dummyimage.com/600x400'},
     {title: 'Mug', link: 'http://dummyimage.com/600x400'}
 ];
 
 const ImageList = () => {
     const RenderedImages = IMAGES.map( function(image) {
-        return <ImageDetail />
+        return <ImageDetail image={image}/>
     });
 
     return (
-        <ul>
+        <ul className="media-list list-group">
             {RenderedImages}
         </ul>
     );
