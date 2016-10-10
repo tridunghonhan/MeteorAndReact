9fb697613b69878ea08f10e849a47d91c5c74618
KennyHo <geotech@KennyMacMini.local>
2016-10-10 17:52:45 +0700

    start to work with Admin LTE on Meteor

---
+++ AdminTLELearn/client/components/header.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class MainHeader extends Component {
+    render(){
+        return(
+            <div>MainHeader</div>
+        );
+    }
+}
+
+export default MainHeader;
